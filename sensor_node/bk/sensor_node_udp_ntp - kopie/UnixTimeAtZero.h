a3513fec8a0a830963a9a4b3255e8d6388df8f6c
Martin Dvořák <dvo0247@vsb.cz>
2025-02-20 07:26:26 +0100

    deleted BKs

--- sensor_node/bk/sensor_node_udp_ntp - kopie/UnixTimeAtZero.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <Arduino.h>
-#include <stdint.h>
-#include <time.h>
-#include "esp32-hal.h"
-
-uint64_t getUnixTimeAtZero(const char* ntpServer1, const char* ntpServer2, const char* ntpServer3);
